c587e193dfd752f91f473abb4c084050f8bc9041
naganu <bstratev@abv.bg>
2016-06-03 21:51:26 +0300

    forget wpi.softPwmCreate

--- incubator/server/routes/dht/diagram.js
+++ incubator/server/routes/dht/diagram.js
@@ -1,8 +1,12 @@
 var wpi = require('wiring-pi');
 var read = require('./bind_and_read');
+var vent = 1;
+var temp = 3;
 wpi.wiringPiSetup();
-wpi.softPwmWrite(1, 50);
-wpi.softPwmWrite(3, 70);
+wpi.softPwmCreate(vent, 0, 100);
+wpi.softPwmCreate(temp, 0, 100);
+wpi.softPwmWrite(vent, 50);
+wpi.softPwmWrite(temp, 70);
 setInterval(function() {
     read().then(function(data) {
         console.log(data.temp);
