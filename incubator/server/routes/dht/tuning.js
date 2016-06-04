5344502c20a185e8d798a9c0515dd4740db50210
naganu <bstratev@abv.bg>
2016-06-04 23:00:41 +0300

    adding vent

--- incubator/server/routes/dht/tuning.js
+++ incubator/server/routes/dht/tuning.js
@@ -1,4 +1,5 @@
 var temp = require('./temp');
+var intVent = require('./int_vent');
 var {Router} = require('express');
 var router = new Router();
 
@@ -14,6 +15,9 @@ router.route('/tuning')
 })
 .post(function(request, response, next) {
     var setTemp = request.body.temp;
+    if(!intVen.interval) {
+        intVent.run();
+    }
     temp.run(setTemp.p, setTemp.i, setTemp.d, setTemp.target, setTemp.period);
     response.send({set: true});
 });
